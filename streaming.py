784d47e88a89a9e23a459b66df582353297ae57d
amrllkmn <35376649+amrllkmn@users.noreply.github.com>
2020-02-09 21:20:55 +0000

    Created the class definition

--- streaming.py
+++ streaming.py
@@ -1,3 +1,25 @@
 #Twitter Web-crawler (for the Web Science coursework)
 #Name: Amirul Lokman Jamaludin
 #GUID: 2259783j
+
+from tweepy import OAuthHandler, Stream, StreamListener
+import tokens as t
+
+consumer_token = t.consumer_key
+consumer_secret = t.consumer_secret
+
+access_token = t.access_token
+access_secret = t.access_token_secret
+
+class StdOutListener(StreamListener):
+    """ A listener handles tweets that are received from the stream.
+    This is a basic listener that just prints received tweets to stdout.
+    """
+    def on_data(self,data):
+        print(data)
+        return True
+    
+    def on_error(self, status):
+        print(status)
+    
+
