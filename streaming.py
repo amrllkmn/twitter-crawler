044a7437055c4bdad39e95b529dab9fdfa4abecc
amrllkmn <35376649+amrllkmn@users.noreply.github.com>
2020-02-13 23:44:54 +0000

    Now stream stores 5 tweets in .json file

--- streaming.py
+++ streaming.py
@@ -4,6 +4,7 @@
 
 from tweepy import OAuthHandler, Stream, StreamListener
 import tokens as t
+import json as j
 
 consumer_token = t.consumer_key
 consumer_secret = t.consumer_secret
@@ -13,26 +14,36 @@ access_secret = t.access_token_secret
 
 class StdOutListener(StreamListener):
     """ A listener handles tweets that are received from the stream.
-    This is a basic listener that just prints received tweets to stdout.
+    The listener collects exactly 5 tweets.
     """
-    def on_status(self, status):
-        #Testing to print a number of tweets
-        i = 0
-        while(i<5):
-            print(status.text)
-            i+=1
-        return False
+    def __init__(self, limit):
+        self.d = {} # empty dictionary to store data
+        self.limit = limit
+        self.file = open("twitter_data.json","w",encoding="utf-8")
+        self.count = 0
 
-    def on_error(self,status_code):
-        if status_code == 420:
+    def on_data(self,data):
+        if self.count < self.limit:
+            self.d[self.count] = data 
+            self.count+=1
+            print(self.count)
+            return True
+        else:
+            j.dump(self.d,self.file,indent=4) #Once done, just dump data in a json file before finishing
+            self.file.close()
             return False
+
+        
+    
+    def on_error(self,status):
+        print(status)
 
 if __name__ == '__main__':
     print("It's streaming...\n")
-    listener = StdOutListener()
+    listener = StdOutListener(5)
     auth = OAuthHandler(consumer_token, consumer_secret)
     auth.set_access_token(access_token, access_secret)
 
     stream = Stream(auth, listener)
-    stream.filter(track=['banana'], is_async=True)    
-# Trying to store it as a JSON file
+    stream.filter(track=['Tesla'], is_async=True)
+    
