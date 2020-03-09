44a195e59b58d3f759b5b85626943828ccd37398
amrllkmn <35376649+amrllkmn@users.noreply.github.com>
2020-03-09 14:56:58 +0000

    removed data_silo

--- data_silo.py
+++
@@ -1,5 +0,0 @@
-import pymongo
-import dns
-
-client = pymongo.MongoClient('mongodb+srv://amrllkmn:TwitterWebCrawling@database-pkr2p.mongodb.net/test?retryWrites=true&w=majority')
-db = client.tweets
