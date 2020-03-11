b93b3f1b636bd73f8d04301549997d94f281c9cd
amrllkmn <35376649+amrllkmn@users.noreply.github.com>
2020-03-11 15:59:55 +0000

    Stream can collect tweets within a given amount of time

--- streaming.py
+++ streaming.py
@@ -6,6 +6,7 @@ from tweepy import OAuthHandler, Stream, StreamListener
 import tokens as t
 import json as j
 import data_silo as d
+import time as tm
 
 consumer_token = t.consumer_key
 consumer_secret = t.consumer_secret
@@ -17,19 +18,31 @@ class StdOutListener(StreamListener):
     """ A listener handles tweets that are received from the stream.
     The listener collects as much tweets as possible
     """
-    def __init__(self):
-        self.count = 0
+    def __init__(self,timelimit=60):
+        self.start = tm.time()
+        self.limit = timelimit
+        super(StdOutListener,self).__init__()
 
     def on_data(self,data):
-        #try:
-            if self.count < 5:
-                j_data = j.loads(data)
-                d.db.altered_carbon.insert_one(j_data)
-                self.count +=1
-                print(self.count)
+        try:
+            if(tm.time() - self.start) < self.limit:
+                d.db.five_min.insert_one(j.loads(data))
                 return True
             else:
                 return False
+        except Exception as e:
+            print(e)
+            return False
+
+        #try:
+        #    if self.count < 5:
+        #        j_data = j.loads(data)
+        #        d.db.altered_carbon.insert_one(j_data)
+        #        self.count +=1
+        #        print(self.count)
+        #        return True
+        #    else:
+        #        return False
         #except Exception as e:
            # print(e)
            # return False
@@ -39,10 +52,11 @@ class StdOutListener(StreamListener):
 
 if __name__ == '__main__':
     print("It's streaming...\n")
-    listener = StdOutListener()
+    listener = StdOutListener(300)
     auth = OAuthHandler(consumer_token, consumer_secret)
     auth.set_access_token(access_token, access_secret)
 
     stream = Stream(auth, listener)
-    stream.filter(track=['Altered Carbon'], is_async=True)
+    stream.filter(track=['COVID-19'], is_async=True)
+    
     
